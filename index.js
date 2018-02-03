6221acb3a822e1ba2445bb848b06c30a2d71c537
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-02-03 08:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+// 1 begins
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten(name){
+kittens.push(name)
+}
+// 2 begins
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+// 3 begins
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+// 4 begins
+function appendKitten(name){
+  appendKitten.splice=(2 , 0 , name);
+}
